11b56d3d1486b80f20b850fad18ef5af0be55f48
david.iniguez.iotmexico <david.iniguez@iotmexico.com>
2022-11-26 11:05:50 -0600

    Fix

--- src/stories.py
+++ src/stories.py
@@ -31,8 +31,6 @@ def health():
 
 @app.route("/story", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def story():
-
-print("No correct print")
     if request.method == 'GET':
         cursor = g.cnx.cursor()
         cursor.execute(f"SELECT ID, Info, Project FROM stories where Project={request.args['project']} AND Evaluated=0 LIMIT 1")
